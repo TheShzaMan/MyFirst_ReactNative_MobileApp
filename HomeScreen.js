07eb5026364c67f9c6164a42fde82349c9f66f09
Shea Raymond <shea.raymond01@gmail.com>
2024-02-02 00:36:51 -0600

    Adding Scrollview and learning how to used. Adding borders to different views to fet a feel for their placement.

--- HomeScreen.js
+++ HomeScreen.js
@@ -3,52 +3,63 @@ import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 const HomeScreen = (navigation) => {
-	const styles = StyleSheet.create({
-		container: {
-			flex: 1,
-			backgroundColor: "#66cdaa",
-			alignItems: "center",
-			justifyContent: "center",
-		},
-		title: {
-			fontSize: 50,
-			color: "#9370db",
-		},
-	});
 	return (
-		<View style={{ flex: 1 }}>
-			<View style={styles.container}>
+		<ScrollView style={styles.container}>
+			<View
+				style={{
+					height: 400,
+					borderStyle: "solid",
+					borderColor: "#00ffff",
+					borderWidth: 2,
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
 				<Text style={styles.title}>My First App</Text>
 				<StatusBar style='auto' />
 			</View>
 			<View
-				style={[
-					styles.container,
-					{
-						position: "absolute",
-						top: "55%",
-						left: "50%",
-					},
-				]}
+				style={{
+					flex: 1,
+					borderStyle: "solid",
+					borderColor: "#00ffff",
+					borderWidth: 2,
+				}}
 			>
 				<Text
 					style={[
-						styles.two,
 						{
 							color: "#daa520",
 							fontSize: 24,
 							backgroundColor: "#5f9ea0",
 							width: "auto",
 							minWidth: 50,
-							alignContent: "stretch",
+							height: 400,
+							textAlign: "center",
 						},
 					]}
 				>
 					by TheShza
 				</Text>
 			</View>
-		</View>
+		</ScrollView>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// height: "100%",
+		backgroundColor: "black",
+		borderStyle: "solid",
+		borderColor: "#00ffff",
+		borderWidth: 2,
+	},
+
+	title: {
+		fontSize: 50,
+		color: "#9370db",
+	},
+});
 
 export default HomeScreen;
